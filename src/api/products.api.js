0d0ae1f48275a646ebaac9390a0c24d059b958dc
arashesmailian <arash.esmailian@gmail.com>
2022-07-04 17:12:38 +0430

    pages added

--- src/api/products.api.js
+++ src/api/products.api.js
@@ -8,10 +8,10 @@ export const getProduct = (id) => {
   });
 };
 
-export const getProducts = () => {
+export const getProducts = (page) => {
   return new Promise((resolve) => {
     fetch(
-      "https://api.digikala.com/v1/search/?page=2&rows=15&price[min]=90000&price[max]=100000&has_selling_stock=1&sort=4&q=سیب/",
+      `https://api.digikala.com/v1/search/?page=${page}&rows=15&price[min]=90000&price[max]=100000&has_selling_stock=1&sort=4&q=سیب/ `,
       {
         method: "GET",
       }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,70 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getProducts } from "../../api/products.api";
-import { HomeBanners, Container, ProductList } from "../../components";
+import {
+  HomeBanners,
+  Container,
+  ProductList,
+  ProductCard,
+} from "../../components";
 import Header from "../../components/Header/Header";
+import InfiniteScroll from "react-infinite-scroll-component";
+import styles from "./home.style.module.css";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const parent = useRef(null);
+  const handleNext = () => {
+    getProducts(page + 1).then((newData) => {
+      setPage(page + 1);
+      setProducts([...products, newData.data.products]);
+    });
+  };
   useEffect(() => {
-    getProducts().then((d) => {
+    getProducts(1).then((d) => {
       setProducts(d.data.products);
     });
   }, []);
+  // getProducts(1).then((d)=>{
+  //   setProducts(d.data.products)
+  // })
   return (
     <div>
       <Header />
       <HomeBanners />
-      <section>
+      <section className={styles.home_products}>
         <Container>
           <ProductList productList={products} />
         </Container>
+        {/* <button
+          className={styles.home_next_page}
+          onClick={() => setPage(page + 1)}
+        >
+          next
+        </button>
+        <button
+          className={styles.home_next_page}
+          onClick={() => setPage(page - 1)}
+        >
+          previous
+        </button> */}
       </section>
+      <Container>
+        <InfiniteScroll
+          dataLength={products.length}
+          next={handleNext}
+          hasMore={true}
+          loader={<h4>loading...</h4>}
+          ref={parent}
+        >
+          {/* <ProductList productList={products} parentRef={parent} /> */}
+          {products.map((product) => (
+            <div key={product.id}>
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </InfiniteScroll>
+      </Container>
     </div>
   );
 };
